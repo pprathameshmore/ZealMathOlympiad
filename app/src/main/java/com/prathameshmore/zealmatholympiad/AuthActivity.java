53b07810b2d13eb98fbb08b7784caa9a816feeb1
Prathamesh More <moreprathameshmore@hotmail.com>
2017-12-29 13:15:23 +0530

    Added Sign In method

--- app/src/main/java/com/prathameshmore/zealmatholympiad/AuthActivity.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/AuthActivity.java
@@ -1,17 +1,24 @@
 package com.prathameshmore.zealmatholympiad;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -23,14 +30,21 @@ public class AuthActivity extends AppCompatActivity {
     private EditText mGetNumber;
 
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mSendBtn = (Button) findViewById(R.id.btn_confirm_mobile_number);
         mGetNumber = (EditText) findViewById(R.id.get_number);
+
+        mSendBtn.isEnabled();
 
 
         mSendBtn.setOnClickListener(new View.OnClickListener() {
@@ -40,13 +54,22 @@ public class AuthActivity extends AppCompatActivity {
                 String phoneNumber = mGetNumber.getText().toString();
 
 
-                PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                        phoneNumber,
-                        60,
-                        TimeUnit.SECONDS,
-                        AuthActivity.this,
-                        mCallbacks
-                );
+                if (mGetNumber.getText().length() == 0) {
+
+                    mGetNumber.setError("Mobile Number can not be empty");
+
+                } else {
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            phoneNumber,
+                            60,
+                            TimeUnit.SECONDS,
+                            AuthActivity.this,
+                            mCallbacks
+                    );
+
+                    Toast.makeText(AuthActivity.this, "Sending...", Toast.LENGTH_SHORT).show();
+                }
+
 
             }
         });
@@ -55,7 +78,7 @@ public class AuthActivity extends AppCompatActivity {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
 
-
+                signInWithPhoneAuthCredential(phoneAuthCredential);
             }
 
             @Override
@@ -64,7 +87,44 @@ public class AuthActivity extends AppCompatActivity {
                 Toast.makeText(AuthActivity.this, "Error while Registration", Toast.LENGTH_SHORT).show();
 
             }
+
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                // ...
+            }
         };
 
     }
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = task.getResult().getUser();
+                            Intent startMainActivity = new Intent(AuthActivity.this, MainActivity.class);
+                            startActivity(startMainActivity);
+                            finish();
+                            // ...
+                        } else {
+                            // Sign in failed, display a message and update the UI
+
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+
 }
