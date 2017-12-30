a64a988e6685a677d99df1ccc4d547fd1e66e3ae
Prathamesh More <moreprathameshmore@hotmail.com>
2017-12-30 08:11:11 +0530

    Added New Activities

---
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/NewsUpdates.java
@@ -0,0 +1,13 @@
+package com.prathameshmore.zealmatholympiad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsUpdates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_updates);
+    }
+}
