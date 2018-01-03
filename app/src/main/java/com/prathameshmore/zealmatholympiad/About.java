57662a5f73eba2c1cec46e8b48d52656b203744c
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-03 11:13:06 +0530

    Our About Page is added with Information

--- app/src/main/java/com/prathameshmore/zealmatholympiad/About.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/About.java
@@ -1,22 +1,39 @@
 package com.prathameshmore.zealmatholympiad;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
 
+import mehdi.sakout.aboutpage.AboutPage;
+
 public class About extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.app_logo)
+                .setDescription("ZEAL MATH OLYMPIAD Version 1.0")
+                .addGroup("Connect with us")
+                .addEmail("moreprathameshmore@hotmail.com","Prathamesh More")
+                .addEmail("vijapuresantosh@gmail.com","Santosh Vijapure")
+                .addWebsite("http://www.about.me/prathameshmore")
+                .addFacebook("pprathameshmore","Prathamesh More")
+                .addFacebook("santoshkumar.vijapure","Santosh Vijapure")
+                .addTwitter("pprathameshmore","Prathamesh More")
+                .addTwitter("vijapure_h","Santosh Vijapure")
+                .addYoutube("UC48QuhKjplEM1cLIPPK-Myw")
+                .addPlayStore("com.ideashower.readitlater.pro")
+                .addGitHub("pprathameshmore","Prathamesh More")
+                .addGitHub("santosh62","Santosh Vijapure")
+                .addInstagram("pprathameshmore","Prathamesh More")
+                .addInstagram("santosh622","Santosh Vijapure")
+                .create();
+        setContentView(aboutPage);
 
     }
-
 }
