0019519ad14c442066aa2dff2484bca15d1cfc03
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-03 11:28:07 +0530

    Minnor Changes are made

--- app/src/main/java/com/prathameshmore/zealmatholympiad/AboutZCOER.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/AboutZCOER.java
@@ -2,6 +2,7 @@ package com.prathameshmore.zealmatholympiad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutZCOER extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class AboutZCOER extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_zcoer);
+
+        View aboutZCOER
+
     }
 }
