7dee21ef15fea5742855a3e9fcbb148945063223
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-04 21:43:47 +0530

    Added Toast Message

--- app/src/main/java/com/prathameshmore/zealmatholympiad/MainActivity.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         _navigation_view = (NavigationView) findViewById(R.id.navigation_view);
 
+        Toast.makeText(this, "This app under development, some feature will not work properly", Toast.LENGTH_SHORT).show();
 
         mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
