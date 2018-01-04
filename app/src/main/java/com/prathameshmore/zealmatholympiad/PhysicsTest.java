91ca0ef3dbc8e26cfb1cfbe359477e35486d79ac
Santosh@62 <vijapuresantosh@gmail.com>
2018-01-04 06:14:46 +0000

    Physics MCQs added

---
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/PhysicsTest.java
@@ -0,0 +1,13 @@
+package com.prathameshmore.zealmatholympiad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhysicsTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_physics_test);
+    }
+}
