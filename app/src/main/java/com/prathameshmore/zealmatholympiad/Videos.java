8b2634c2baafacab256d66ad0bd4c2d9f1aff892
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-05 13:14:18 +0530

    Now You can Sign In properly

--- app/src/main/java/com/prathameshmore/zealmatholympiad/Videos.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/Videos.java
@@ -1,11 +1,7 @@
 package com.prathameshmore.zealmatholympiad;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,13 +10,10 @@ public class Videos extends AppCompatActivity {
 
     private WebView mWebView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         mWebView = (WebView) findViewById(R.id.web_view);
 
@@ -31,7 +24,5 @@ public class Videos extends AppCompatActivity {
 
         mWebView.setWebViewClient(new WebViewClient());
 
-
     }
-
 }
