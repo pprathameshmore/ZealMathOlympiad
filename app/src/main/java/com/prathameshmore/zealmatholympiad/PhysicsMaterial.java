10d6950d8a7da07e04449198c7968929c17df51e
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-16 19:12:49 +0530

    ID name changed

--- app/src/main/java/com/prathameshmore/zealmatholympiad/PhysicsMaterial.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/PhysicsMaterial.java
@@ -3,23 +3,30 @@ package com.prathameshmore.zealmatholympiad;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.TextView;
 
 public class PhysicsMaterial extends AppCompatActivity {
 
     private TextView textViewPhysicsPDF;
-    private TextView textViewPractisePhysicsTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_physics_material);
 
-        textViewPhysicsPDF = (TextView) findViewById(R.id.physicsPDF);
-        textViewPractisePhysicsTest = (TextView) findViewById(R.id.practisePhysicsTest);
+        textViewPhysicsPDF = (TextView) findViewById(R.id.pdf_physics);
 
 
+
+        textViewPhysicsPDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startPhysicsPDFActivity = new Intent(PhysicsMaterial.this,PhysicsPDF.class);
+                startActivity(startPhysicsPDFActivity);
+            }
+        });
 
     }
 }
