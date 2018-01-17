8c3274a6fe52654426a2e2f0fc538e4a6684ce14
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-17 22:44:23 +0530

    Added Expandable TextView Library

--- app/src/main/java/com/prathameshmore/zealmatholympiad/Events.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/Events.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+
 public class Events extends AppCompatActivity {
 
 
@@ -14,6 +16,10 @@ public class Events extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        String randomText = "Fat new smallness few supposing suspicion two. Course sir people worthy horses add entire suffer. How one dull get busy dare far. At principle perfectly by sweetness do. As mr started arrival subject by believe. Strictly numerous outlived kindness whatever on we no on addition";
+        ExpandableTextView expandableTextView = (ExpandableTextView) findViewById(R.id.expand_text_view);
+        expandableTextView.setText(randomText);
 
     }
 }
