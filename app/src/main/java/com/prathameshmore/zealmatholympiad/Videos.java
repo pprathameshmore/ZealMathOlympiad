62255154c7325cb510fce31505916b5106cdcde5
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-19 07:14:01 +0530

    Added ActionFloating Button in Videos Activity

--- app/src/main/java/com/prathameshmore/zealmatholympiad/Videos.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/Videos.java
@@ -1,7 +1,11 @@
 package com.prathameshmore.zealmatholympiad;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -9,6 +13,7 @@ import android.webkit.WebViewClient;
 public class Videos extends AppCompatActivity {
 
     private WebView mWebView;
+    private FloatingActionButton floatingActionButtonYouTubeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,7 @@ public class Videos extends AppCompatActivity {
         setContentView(R.layout.activity_videos);
 
         mWebView = (WebView) findViewById(R.id.web_view);
+        floatingActionButtonYouTubeBtn = (FloatingActionButton) findViewById(R.id.fab);
 
 
         WebSettings myWebSettings = mWebView.getSettings();
@@ -23,6 +29,15 @@ public class Videos extends AppCompatActivity {
         mWebView.loadUrl("https://www.youtube.com/channel/UCzfVyPDkyEEE4xkLrXYC86g");
 
         mWebView.setWebViewClient(new WebViewClient());
+
+        floatingActionButtonYouTubeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startInYouTue = new Intent(Intent.ACTION_VIEW);
+                startInYouTue.setData(Uri.parse("https://www.youtube.com/channel/UCzfVyPDkyEEE4xkLrXYC86g"));
+                startActivity(startInYouTue);
+            }
+        });
 
     }
 }
