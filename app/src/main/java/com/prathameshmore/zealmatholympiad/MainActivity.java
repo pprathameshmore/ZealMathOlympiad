96f1b20d8afeec43abb24f0f1c6b60550fb8191e
Prathamesh More <moreprathameshmore@hotmail.com>
2018-01-26 13:17:47 +0530

    Uncomment onStart() method

--- app/src/main/java/com/prathameshmore/zealmatholympiad/MainActivity.java
+++ app/src/main/java/com/prathameshmore/zealmatholympiad/MainActivity.java
@@ -169,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-/*
+
 
     public void onStart() {
         super.onStart();
@@ -184,7 +184,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-*/
 
 
 }
